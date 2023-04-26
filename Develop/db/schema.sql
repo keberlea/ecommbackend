40d04259fd5028d05e4fd332f856cd70c23697e2
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-26 14:26:48 -0700

    updated schema added code to models folder files

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,37 +1,28 @@
--- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
-
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
--- create catergory table
 CREATE TABLE category (
     id INT NOT NULL AUTO_INCREMENT,
-    category_name STRING NOT NULL,
+    category_name VARCHAR NOT NULL,
     PRIMARY KEY (id)
 );
 
--- create product table
 CREATE TABLE product (
     id INT NOT NULL AUTO_INCREMENT,
-    product_name STRING NOT NULL,
-    price DECIMAL NOT NULL, -- validate it is a decimal
-    stock INT NOT NULL, -- validate numeric
-    -- set default of category_id to default 10
-    category_id INT DEFAULT 10,
+    product_name VARCHAR(50) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL,
+    category_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-
--- create tag table
 CREATE TABLE tag (
     id INT NOT NULL AUTO_INCREMENT,
-    tag_name STRING NOT NULL,
+    tag_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
--- create product tag table
 CREATE TABLE product_tag (
     id INT NOT NULL AUTO_INCREMENT,
     product_id INT NOT NULL,
